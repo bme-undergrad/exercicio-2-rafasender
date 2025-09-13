57871a2a557f2f0050022a14425fc5a1912ae223
rafasender <rafasender2006@gmail.com>
2025-09-13 18:32:11 -0300

    commit3

--- exercicio2.m
+++ exercicio2.m
@@ -10,12 +10,10 @@ pkg load optim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
 % 1. Vetor de custos (funcao objetivo)
 c = [10; (2 * NG)];
 
 % 2. Matriz de restricoes de desigualdade (A*x <= b)
-% Note que multiplicamos por -1 para inverter o sinal de >= para <=
 A = [-0.30, -0.20;  % Restricao de Polietileno
      -0.20, -0.25;  % Restricao de PLA
      -0.25, -0.30]; % Restricao de Polipropileno
@@ -27,9 +25,8 @@ b = [-15; -10; -12];
 lb = [0; 0];
 
 % 5. Resolvendo o problema de programacao linear
-% Nao temos restricoes de igualdade (Aeq, beq) nem limites superiores (ub)
-% A funcao linprog retorna o vetor 'x' com as quantidades otimas
-x = linprog(c, A, b, [], [], lb);
+% A correção está aqui: adicionamos '[]' para o argumento 'ub' (limite superior)
+x = linprog(c, A, b, [], [], lb, []);
 
 % mantenha essas duas linhas finais
 blendA = x(1);
